75103ffcd63fac6999d41e6d661af476a2499dd2
oli-pad <oliver.oakes@people-discover.com>
2023-09-27 10:59:40 +0100

    cleaning

--- src/main.py
+++ src/main.py
@@ -1,20 +1,11 @@
-from modules.SuperdrugInvoiceDetail import SuperdrugInvoiceDetail
-from modules.ProduceFileList import ProduceFileList
-from modules.RenameInvoices import RenameInvoices
 from modules.Get_Scrubbed_Detail import Get_Scrubbed_Detail
 from modules.InsertIntoSQL import InsertIntoSQL
-import pandas as pd
 
-#RenameInvoices('W:\Audit\Coty\Invoice Images\EmailStagingBay\Superdrug')
-
-#print(SuperdrugInvoiceDetail('W:\Audit\Coty\Invoice Images\ImageStagingBay\Superdrug\\5944701.pdf','CL027','SUP01').Line_Description())
-
-#df = SuperdrugInvoiceDetail('W:\Audit\Coty\Invoice Images\ImageStagingBay\Superdrug\\5438418.pdf','CL027','SUP01').Full_Invoice()
-
-#print(df[["Line_Description","Product_No", "Net_Amount", "VAT_Amount", "Gross_Amount"]])
-#print(df["Gross_Amount"].sum())
-
+#Stores all the invoice data in a local dataframe
 df = Get_Scrubbed_Detail('W:\Audit\Coty\Invoice Images\ImageStagingBay\Superdrug','CL027','SUP01')
 
+#Inserts the data into the SQL database
 InsertIntoSQL(df,'CL027','SUP01')
+
+#For faster testing CSV to check the data in the dataframe
 df.to_csv('W:\Audit\Coty\Invoice Images\Superdrug.csv',index=False)
